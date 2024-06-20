9a25873b449e7b904c3e16418aa345ae13b4b69d
ozen-hub <info.ozenhub@gmail.com>
2024-06-20 21:02:09 +0530

    init

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {
   CustomerStatusManagerComponent
 } from "../customers/inner/customer-status-manager/customer-status-manager.component";
@@ -9,6 +9,10 @@ import {MatDialog} from "@angular/material/dialog";
 import {NewProductComponent} from "./inner-pages/new-product/new-product.component";
 import {UpdateProductComponent} from "./inner-pages/update-product/update-product.component";
 import {ManageProductImagesComponent} from "./inner-pages/manage-product-images/manage-product-images.component";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {debounceTime} from "rxjs";
+import {ProductService} from "../../service/product/product.service";
+import {CurrencyPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-products',
@@ -17,57 +21,92 @@ import {ManageProductImagesComponent} from "./inner-pages/manage-product-images/
     CustomerStatusManagerComponent,
     MatIcon,
     MatIconButton,
-    MatTooltip
+    MatTooltip,
+    ReactiveFormsModule,
+    NgForOf,
+    CurrencyPipe
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
-  constructor(private matDialog: MatDialog) {
+export class ProductsComponent implements OnInit {
+  searchText = '';
+  page: any = 0;
+  size: any = 10;
+  count=0;
+  dataList:any[]=[];
+
+  searchForm: FormGroup = new FormGroup({
+    text: new FormControl('')
+  });
+
+  constructor(private matDialog: MatDialog, private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+
+    this.loadAllProducts();
+
+    this.searchForm.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(data => {
+        this.searchText = data.text;
+        this.loadAllProducts();
+      })
   }
 
   openNewProductForm() {
-    let matDialogRef = this.matDialog.open(NewProductComponent,{
-      width:'500px',
-      disableClose:true
+    let matDialogRef = this.matDialog.open(NewProductComponent, {
+      width: '500px',
+      disableClose: true
     });
 
-    matDialogRef.afterClosed().subscribe(response=>{
-      if(response){
+    matDialogRef.afterClosed().subscribe(response => {
+      if (response) {
         this.loadAllProducts();
       }
     })
   }
-  openUpdateProductForm(product:any) {
-    let matDialogRef = this.matDialog.open(UpdateProductComponent,{
-      width:'500px',
-      disableClose:true,
-      data:product
+
+  openUpdateProductForm(product: any) {
+    let matDialogRef = this.matDialog.open(UpdateProductComponent, {
+      width: '500px',
+      disableClose: true,
+      data: product
     });
 
-    matDialogRef.afterClosed().subscribe(response=>{
-      if(response){
+    matDialogRef.afterClosed().subscribe(response => {
+      if (response) {
         this.loadAllProducts();
       }
     })
   }
-  openProductImageForm(product:any) {
-    let matDialogRef = this.matDialog.open(ManageProductImagesComponent,{
-      width:'500px',
-      disableClose:true,
-      data:product
+
+  openProductImageForm(product: any) {
+    let matDialogRef = this.matDialog.open(ManageProductImagesComponent, {
+      width: '500px',
+      disableClose: true,
+      data: product
     });
 
-    matDialogRef.afterClosed().subscribe(response=>{
-      if(response){
+    matDialogRef.afterClosed().subscribe(response => {
+      if (response) {
         this.loadAllProducts();
       }
     })
   }
 
   private loadAllProducts() {
-
+    this.productService.search(this.page, this.size, this.searchText).subscribe(response => {
+      console.log(response);
+      this.dataList = response.data?.dataList;
+      this.count = response.data?.count;
+    });
   }
 
   protected readonly confirm = confirm;
+
+  deleteConfirm(item: any) {
+    
+  }
 }
--- src/app/service/product/product.service.ts
+++ src/app/service/product/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +17,14 @@ export class ProductService {
       unitPrice:obj.unitPrice,
       description:obj.description
     })
+  }
+
+  search(page:any, size:any, searchText:any):Observable<any>{
+    let params = new HttpParams();
+    params=params.append('searchText', searchText);
+    params=params.append('page', page);
+    params=params.append('size', size);
+    return this.http.get(this.baseUrl+'/list',{params:params});
   }
 
 }
